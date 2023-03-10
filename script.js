297073afc0602ad55d56a24637d3be9fc1a721ef
madiscercy <madiscercy@gmail.com>
2023-03-09 22:55:13 -0500

    added if statements for every array

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ let lowerCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","
 let upperCharacters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
 'Y', 'Z' ];
-let numCharacters = ["1","2","3","4","5","6","7","8","9",];
-let specialChar = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|","\",","<",",",">",".","?","/"];
+let numCharacters = ["1","2","3","4","5","6","7","8","9","0"];
+let specialCharacters = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|","\",","<",",",">",".","?","/"];
 
 // Write password to the #password input
 function writePassword() {
-  console.log("a" + "a" + "a")
   let password = generatePassword();
-
+  console.log(password);
   if (password !== "") {
     let passwordText = document.querySelector("#password");
     passwordText.value = password;
@@ -42,14 +41,40 @@ function generatePassword() {
   } 
 
   let password = "";
+
   for ( i = 0; i < passwordLength; i++ ){
-    password = password + Math.random();
+    let charArray = []
+    if (lowercase) {
+      let randomIndex = (Math.floor(Math.random() * (lowerCharacters.length)));
+      let lowerChar = lowerCharacters[randomIndex]; 
+      charArray.push(lowerChar)
+    }
+    
+    if (uppercase) {
+      let randomIndex = (Math.floor(Math.random() * (upperCharacters.length)));
+      let upperChar = upperCharacters[randomIndex]; 
+      charArray.push(upperChar)
+    }
+
+    if (numeric) {
+      let randomIndex = (Math.floor(Math.random() * (numCharacters.length)));
+      let numericChar = numCharacters[randomIndex]; 
+      charArray.push(numericChar)
+    }
+
+    if (specialCharacter) {
+      let randomIndex = (Math.floor(Math.random() * (specialCharacters.length)));
+      let specialChar = specialCharacters[randomIndex]; 
+      charArray.push(specialChar)
+    }
+
+    let randomCharIndex = (Math.floor(Math.random() * (charArray.length)));
+    let randomChar = charArray[randomCharIndex];
+    password = password + randomChar;
     console.log(password);
   }
   console.log(password);
   return password;
-
-  // for loop here
 }
 
 // for loop to repeat process however many times needed, random logic to get random characters, concact to build array of random to use
